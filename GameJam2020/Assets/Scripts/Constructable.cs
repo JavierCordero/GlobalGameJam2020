d24885aee4e266716e92eb507870491eb79aaee1
Kelvin-CD <kelvinco@ucm.es>
2020-02-01 00:48:06 +0100

    Player can now plant seeds

---
+++ GameJam2020/Assets/Scripts/Constructable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constructable : Interactable
+{
+    [SerializeField] private ItemType itemNeeded;
+    [SerializeField] private GameObject objectPrefab;
+
+    public override void Interact()
+    {
+        if (PlayerController.Instance.GetCurrentItem().GetItemType() == itemNeeded)
+        {
+            PlayerController.Instance.ClearHand();
+            Transform constructedObject =  Instantiate(objectPrefab).transform;
+            constructedObject.position = transform.position;
+            Destroy(this.gameObject);
+        }
+    }
+}
