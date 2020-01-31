82d0268e9dcac20ae29a76a08d167802cccf6349
Javier Cordero <javiercorderocalvo@gmail.com>
2020-02-01 00:05:44 +0100

    Terminado lo e quitar el suelo y MERGE

---
+++ GameJam2020/Assets/Scenes/Test/FloorScene/Scripts/NumOfZonesCleaned.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumOfZonesCleaned : MonoBehaviour
+{
+	private int numOfZonesCleaned = 0, totalNumOfZonesCleaned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		totalNumOfZonesCleaned = FindObjectsOfType<ChangeMyZoneScript>().Length;
+	}
+
+	public void increaseNumberOfZonesCleaned(GameObject obj)
+	{
+		numOfZonesCleaned++;
+
+		if (numOfZonesCleaned < totalNumOfZonesCleaned)
+			obj.GetComponent<ChangeMyZoneScript>().despoblateZone();
+	}
+
+	public void decreaseNumberOfZonesCleaned()
+	{
+		numOfZonesCleaned--;
+	}
+}
