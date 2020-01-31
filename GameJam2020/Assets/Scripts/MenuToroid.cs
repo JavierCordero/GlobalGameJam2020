dd0d26bbbe3393cd4da2363577a5f2d01098e77a
Rodrigo Manuel Pérez Ruiz <rodrigo.manuel.perez@gmail.com>
2020-02-01 00:38:31 +0100

    Level Selector Scene

---
+++ GameJam2020/Assets/Scripts/MenuToroid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuToroid : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public Transform playerTransform;
+
+    public Vector3 offset;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cameraTransform.transform.position += offset;
+        playerTransform.transform.position += offset;
+    }
+}
