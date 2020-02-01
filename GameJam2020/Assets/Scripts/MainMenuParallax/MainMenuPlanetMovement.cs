fb37c625ae3b4efb195339ea4682b66742272340
Alejandro Villar Rubio <avr-98@hotmail.es>
2020-02-01 21:02:36 +0100

    Arreglado un fallo en el parallax del menu

--- GameJam2020/Assets/Scripts/MainMenuParallax/MainMenuPlanetMovement.cs
+++ GameJam2020/Assets/Scripts/MainMenuParallax/MainMenuPlanetMovement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class MainMenuPlanetMovement : MonoBehaviour
 {
+    float velRot;
     [HideInInspector]
     public float velocity;
+
+    private void Start()
+    {
+        velRot = Random.Range(0f, 10f);
+    }
     void Update()
     {
-        transform.Translate(Vector3.right * velocity * Time.deltaTime);
+        transform.Translate(Vector3.right * velocity * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.up * -velRot * Time.deltaTime, Space.World);
     }
+
+
 }
