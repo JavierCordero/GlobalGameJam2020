5b8d00624a528a09be47db4b18a084c77d14fd8d
Rodrigo Manuel Pérez Ruiz <rodrigo.manuel.perez@gmail.com>
2020-02-01 22:10:38 +0100

    Credits on

---
+++ GameJam2020/Assets/Scripts/CreditsScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour
+{
+
+    float scrollSpeed = 5f;
+    float scrollTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, scrollSpeed, 0) * Time.deltaTime;
+    }
+}
