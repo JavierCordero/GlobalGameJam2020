360d0d0e02bf2f2718cba8c86a27482d686cd83e
Rodrigo Manuel Pérez Ruiz <rodrigo.manuel.perez@gmail.com>
2020-02-01 03:33:23 +0100

    Level selector manager

---
+++ GameJam2020/Assets/Scripts/LevelSelectorPlayer/LevelMenuManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenuManager : MonoBehaviour
+{
+    // Cambio de escena
+    // Bloqueo desbloqueo de nivels disponibles
+    // Salir al menu inicial
+    // 
+}
