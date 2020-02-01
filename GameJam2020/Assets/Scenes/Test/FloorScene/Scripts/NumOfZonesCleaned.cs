46de9fa86275885d9b85780cf1f359bb66e025e1
Javier Cordero <ruiderillo@hotmail.es>
2020-02-01 15:55:58 +0100

    Level 1 terminado jj

--- GameJam2020/Assets/Scenes/Test/FloorScene/Scripts/NumOfZonesCleaned.cs
+++ GameJam2020/Assets/Scenes/Test/FloorScene/Scripts/NumOfZonesCleaned.cs
@@ -7,17 +7,9 @@ public class NumOfZonesCleaned : MonoBehaviour
 	private int numOfZonesCleaned = 0;
 	public int totalNumOfZonesCleaned = 0;
 
-	public void increaseNumberOfZonesCleaned(GameObject obj)
+	public void checkIfLevelCompleted()
 	{
-		numOfZonesCleaned++;
-
-		Debug.Log(numOfZonesCleaned);
-
-		if (numOfZonesCleaned < totalNumOfZonesCleaned)
-		{
-			obj.GetComponent<ChangeMyZoneScript>().despoblateZone();
-		}
-		else if (numOfZonesCleaned >= totalNumOfZonesCleaned && totalNumOfZonesCleaned > 1)
+		if (numOfZonesCleaned + 1 >= totalNumOfZonesCleaned && totalNumOfZonesCleaned > 1)
 		{
 			ChangeMyZoneScript[] cm = FindObjectsOfType<ChangeMyZoneScript>();
 
@@ -25,9 +17,31 @@ public class NumOfZonesCleaned : MonoBehaviour
 			{
 				c.poblateZone();
 			}
+
+			Destroy(this);
+		}
+	}
+
+	public void increaseNumberOfZonesCleaned(GameObject obj)
+	{
+		numOfZonesCleaned++;
+
+		if (numOfZonesCleaned < totalNumOfZonesCleaned)
+		{
+			obj.GetComponent<ChangeMyZoneScript>().despoblateZone();
 		}
 
-		Destroy(this);
+		//else if (numOfZonesCleaned >= totalNumOfZonesCleaned && totalNumOfZonesCleaned > 1)
+		//{
+		//	ChangeMyZoneScript[] cm = FindObjectsOfType<ChangeMyZoneScript>();
+
+		//	foreach (ChangeMyZoneScript c in cm)
+		//	{
+		//		c.poblateZone();
+		//	}
+		//}
+
+		//Destroy(this);
 	}
 
 	public void decreaseNumberOfZonesCleaned()
