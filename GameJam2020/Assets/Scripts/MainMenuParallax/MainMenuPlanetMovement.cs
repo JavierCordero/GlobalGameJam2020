bcfad9eb749ff806898b311fdf075e38155ac5a4
ssjsrgx <ssjsrgx@gmail.com>
2020-02-01 15:35:44 +0100

    carpeta para alejandro

---
+++ GameJam2020/Assets/Scripts/MainMenuParallax/MainMenuPlanetMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPlanetMovement : MonoBehaviour
+{
+    public float velocity;
+    void Update()
+    {
+        transform.Translate(Vector3.right * velocity * Time.deltaTime);
+    }
+}
