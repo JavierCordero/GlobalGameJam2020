6413314adc294229a6b5874e89472a8534761fdf
Kelvin-CD <kelvinco@ucm.es>
2020-02-01 11:45:29 +0100

    Bucket item added, can fill it with water

---
+++ GameJam2020/Assets/Scripts/Bucket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : Item
+{
+    [SerializeField] private Mesh fullBucket;
+
+    private Mesh emptyBucket;
+    private bool bucketIsFull = false;
+    private MeshFilter meshFilter;
+
+    public bool IsBucketFull() { return bucketIsFull; }
+
+    void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        emptyBucket = meshFilter.mesh;
+    }
+
+    public void TryFillBucket()
+    {
+        if (!bucketIsFull)
+        {
+            meshFilter.mesh = fullBucket;
+            bucketIsFull = true;
+        }
+    }
+
+    public void TryEmptyBucket()
+    {
+        if (bucketIsFull)
+        {
+            meshFilter.mesh = emptyBucket;
+            bucketIsFull = false;
+        }
+    }
+}
--- GameJam2020/Assets/Scripts/Item.cs
+++ GameJam2020/Assets/Scripts/Item.cs
@@ -6,6 +6,7 @@ public enum ItemType
 {
     Wood,
     TreeSappling,
+    Bucket,
     None
 }
 
