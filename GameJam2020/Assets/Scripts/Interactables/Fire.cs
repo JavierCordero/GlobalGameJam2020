f1cbbcd334e4db59d4fc2dca2fea240c8e6adff1
Kelvin-CD <kelvinco@ucm.es>
2020-02-02 02:41:14 +0100

    Fire behaviour

---
+++ GameJam2020/Assets/Scripts/Interactables/Fire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : Interactable
+{
+    public override void Interact()
+    {
+        if (PlayerController.Instance.HasItem() && PlayerController.Instance.GetCurrentItem().GetItemType() == ItemType.Bucket)
+        {
+            Bucket bucket = (Bucket)PlayerController.Instance.GetCurrentItem();
+            if (bucket.IsBucketFull())
+            {
+                bucket.TryEmptyBucket();
+                //LevelManager.Instance.PerformAction(ActionType.WaterTree);
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
