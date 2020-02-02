e2a0e9c743407978a1c45b5edccc668e5b3f7ff3
ssjsrgx <ssjsrgx@gmail.com>
2020-02-02 13:13:53 +0100

    arreglado fade

--- GameJam2020/Assets/Scripts/LevelSelectorPlayer/LevelSelectorCircleInOut.cs
+++ GameJam2020/Assets/Scripts/LevelSelectorPlayer/LevelSelectorCircleInOut.cs
@@ -14,7 +14,7 @@ public class LevelSelectorCircleInOut : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("circleOut");
+        //StartCoroutine("circleOut");
     }
 
     public void sceneOut()
